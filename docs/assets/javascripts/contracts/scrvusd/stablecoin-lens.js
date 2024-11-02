1505cc88fd47cea745265135bff00dc47de3fb31
mo <moanonresearch@gmail.com>
2024-11-02 09:24:54 +0100

    script: fix naming

--- docs/assets/javascripts/contracts/scrvusd/stablecoin-lens.js
+++ docs/assets/javascripts/contracts/scrvusd/stablecoin-lens.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', async function() {
     const web3 = new Web3('https://eth.llamarpc.com');
-    const FeeSplitterAddress = '0xe24e2db9f6bb40bbe7c1c025bc87104f5401ecd7';
+    const StablecoinLensAddress = '0xe24e2db9f6bb40bbe7c1c025bc87104f5401ecd7';
 
-    const FeeSplitterABI = [{"inputs":[],"name":"circulating_supply","outputs":[{"name":"","type":"uint256"}],"stateMutability":"view","type":"function"}];
+    const StablecoinLensABI = [{"inputs":[],"name":"circulating_supply","outputs":[{"name":"","type":"uint256"}],"stateMutability":"view","type":"function"}];
     
-    const FeeSplitterContract = new web3.eth.Contract(FeeSplitterABI, FeeSplitterAddress);
+    const StablecoinLensContract = new web3.eth.Contract(StablecoinLensABI, StablecoinLensAddress);
     
     async function updateValues() {
         try {
-            const result = await FeeSplitterContract.methods.circulating_supply().call();
+            const result = await StablecoinLensContract.methods.circulating_supply().call();
             const element = document.getElementById('circulatingSupplyOutput'); // Make sure this ID exists in your HTML
             if (element) {
                 element.textContent = result.toString();
